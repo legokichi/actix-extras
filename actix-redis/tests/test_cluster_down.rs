cedeb2dd53c5a327ac381ae533c15b21b68792c6
pandaman64 <kointosudesuyo@infoseek.jp>
2020-05-28 08:10:19 +0900

    simpler test for scaling down cluster nodes

--- actix-redis/tests/test_cluster_down.rs
+++ actix-redis/tests/test_cluster_down.rs
@@ -14,20 +14,36 @@ fn success<T: Debug, E1: Debug, E2: Debug>(res: Result<Result<T, E1>, E2>) -> T 
 async fn test_cluster_scaledown() {
     env_logger::init();
 
-    let addr = RedisClusterActor::start("redis.service.dc1.consul:16379");
-    success(
-        addr.send(Get {
-            key: "shutdown".into(),
-        })
-        .await,
+    let addr = RedisClusterActor::start("127.0.0.1:7000");
+
+    let key = "test-cluster-scaledown";
+    let target_slot = actix_redis::slot::slot(key.as_bytes());
+
+    // target_slot must be served at first
+    assert!(success(addr.send(set(key, "value")).await));
+    assert_eq!(
+        success(addr.send(get(key)).await),
+        Some(b"value"[..].into())
     );
-    success(addr.send(set("shutdown", "1")).await);
-    // 2 x cluster-node-timeout + 1
-    delay_for(Duration::from_secs(3)).await;
-    success(
-        addr.send(Get {
-            key: "shutdown".into(),
+
+    // shut down the master node serving the key
+    // TODO: restart the node after the test
+    assert!(addr
+        .send(DirectedTo {
+            command: shutdown(),
+            slot: target_slot,
         })
-        .await,
+        .await
+        .unwrap()
+        .is_err());
+
+    // wait until the cluster agrees upon the promotion
+    // 2 x cluster-node-timeout + 1 sec
+    delay_for(Duration::from_secs(3)).await;
+
+    // make sure that the client can access after the promotion
+    assert_eq!(
+        success(addr.send(get(key)).await),
+        Some(b"value"[..].into())
     );
 }
